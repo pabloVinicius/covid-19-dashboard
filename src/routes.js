4a71352b85dcd15f06385f4c954964ca46456ad3
Pablo Vinícius <vinicius.pablo.18@gmail.com>
2020-03-18 18:24:16 -0300

    adding redirect to routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Index from './pages/Index';
 import Daily from './pages/Daily';
@@ -10,6 +10,7 @@ export default () => {
       <Switch>
         <Route path="/" exact component={Index} />
         <Route path="/daily" component={Daily} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   );
